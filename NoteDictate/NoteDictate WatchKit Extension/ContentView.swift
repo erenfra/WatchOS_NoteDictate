da9a037ed33f9b277621c8cd9b637534d6147877
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-06-29 18:02:45 -0500

    first commit

---
+++ NoteDictate/NoteDictate WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoteDictate WatchKit Extension
+//
+//  Created by Renato Oliveira Fraga on 6/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoteDictate/NoteDictate WatchKit Extension/NoteDictateApp.swift
@@ -0,0 +1,19 @@
+//
+//  NoteDictateApp.swift
+//  NoteDictate WatchKit Extension
+//
+//  Created by Renato Oliveira Fraga on 6/29/21.
+//
+
+import SwiftUI
+
+@main
+struct NoteDictateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
